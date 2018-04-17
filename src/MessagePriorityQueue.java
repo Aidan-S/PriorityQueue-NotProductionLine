3e93a91398423bdde91802350248c02d3ee75a76
scannella <scannella@bishopireton.org>
2018-04-17 12:21:05 -0400

    trying to figure out the structure of the priority queue

--- src/MessagePriorityQueue.java
+++ src/MessagePriorityQueue.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+import java.util.Queue;
 
 public class MessagePriorityQueue{
 
+	private ArrayList<Queue> listOfQueues;
 	
 	
-	
-	
+	public MessagePriorityQueue() {
+		listOfQueues
+	}
 	
 	
 	
