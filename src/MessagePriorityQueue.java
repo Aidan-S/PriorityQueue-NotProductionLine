08e3fbe8d30e6f2e850191e60f9a1059882e4d93
scannella <scannella@bishopireton.org>
2018-04-20 10:00:17 -0400

    need to ask prof kelly about how to do the arrival time stuff

--- src/MessagePriorityQueue.java
+++ src/MessagePriorityQueue.java
@@ -1,15 +1,25 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Queue;
+
 
 public class MessagePriorityQueue{
 
-	private ArrayList<Queue> listOfQueues;
+	private ArrayList<Queue> priorityQueue;
 	
 	
 	public MessagePriorityQueue() {
-		listOfQueues = new ArrayList<Queue>(5);
-		Queue <Message> listZero = new Queue<Message>();
-		listOfQueues.set(0, listZero);
+		priorityQueue = new ArrayList<Queue>(5);
+		Queue <Message> listZero = new LinkedList<Message>();
+		priorityQueue.set(0, listZero);
+		Queue <Message> listOne = new LinkedList<Message>();
+		priorityQueue.set(1, listOne);
+		Queue <Message> listTwo = new LinkedList<Message>();
+		priorityQueue.set(2, listTwo);
+		Queue <Message> listThree = new LinkedList<Message>();
+		priorityQueue.set(3, listThree);
+		Queue <Message> listFour = new LinkedList<Message>();
+		priorityQueue.set(4, listFour);
 	}
 	
 	
