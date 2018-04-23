14cc06c0b359d9362b4d0712d7153ed8c2377b7b
scannella <scannella@bishopireton.org>
2018-04-23 15:00:23 -0400

    Fix your stuffs ya nub

--- src/MessagePriorityQueue.java
+++ src/MessagePriorityQueue.java
@@ -4,7 +4,7 @@ import java.util.Queue;
 
 //
 public class MessagePriorityQueue{
-
+ 
 	private ArrayList<Queue> priorityQueue;
 	private int arrivalTime;
 	
@@ -38,6 +38,14 @@ public class MessagePriorityQueue{
 		}
 	}
 	
+	public boolean isEmpty() {
+		if(!priorityQueue.get(4).isEmpty() && !priorityQueue.get(3).isEmpty() && !priorityQueue.get(2).isEmpty() && !priorityQueue.get(1).isEmpty() && !priorityQueue.get(0).isEmpty()) {
+			return true;
+		}
+		return false;
+	}
+	
+	
 	public Message remove() {
 		int i = -1;
 		if(!priorityQueue.get(4).isEmpty()) {
@@ -68,7 +76,7 @@ public class MessagePriorityQueue{
         case 4: arrivalTime += 4; 
         		return (Message) priorityQueue.get(4).remove();
         default: System.out.println("All messages processed");
-        		 return null;	
+        		return null;	
 	
 		}
 	}
@@ -76,25 +84,79 @@ public class MessagePriorityQueue{
 	
 	public static void main (String[] args) {
 		MessagePriorityQueue q = new MessagePriorityQueue();
-		Message m4 = new Message(4, "Four");
-		Message m3 = new Message(3, "Third");
-		Message m2 = new Message(2, "Second");
-		Message m1 = new Message(1, "First");
+		Message m41 = new Message(4, "Four 1");
+		Message m42 = new Message(4, "Four 2");
+		Message m43 = new Message(4, "Four 3");
+		Message m44 = new Message(4, "Four 4");
 		
-		q.add(m2);
-		q.add(m1);
-		q.add(m4);
-		q.add(m3);
+		Message m31 = new Message(3, "Third 1");
+		Message m32 = new Message(3, "Third 2");
+		Message m33 = new Message(3, "Third 3");
+		Message m34 = new Message(3, "Third 4");
+		
+		Message m21 = new Message(2, "Second 1");
+		Message m22 = new Message(2, "Second 2");
+		Message m23 = new Message(2, "Second 3");
+		Message m24 = new Message(2, "Second 4");
+		
+		Message m11 = new Message(1, "First 1");
+		Message m12 = new Message(1, "First 2");
+		Message m13 = new Message(1, "First 3");
+		Message m14 = new Message(1, "First 4");
+		
+		Message m01 = new Message(0, "Zero 1");
+		Message m02 = new Message(0, "Zero 2");
+		Message m03 = new Message(0, "Zero 3");
+		Message m04 = new Message(0, "Zero 4");
+		
+		q.add(m41);
+		q.add(m42);
+		q.add(m43);
+		q.add(m44);
+		q.add(m34);
+		q.add(m33);
+		q.add(m32);
+		q.add(m31);
+		q.add(m24);
+		q.add(m23);
+		q.add(m22);
+		q.add(m21);
+		q.add(m14);
+		q.add(m13);
+		q.add(m12);
+		q.add(m11);
+		q.add(m04);
+		q.add(m03);
+		q.add(m02);
+		q.add(m01);
 		
 		System.out.println(q.remove());
 		System.out.println(q.remove());
 		System.out.println(q.remove());
 		System.out.println(q.remove());
+		System.out.println(q.remove());
+		System.out.println(q.remove());
+		System.out.println(q.remove());
+		System.out.println(q.remove());
+		System.out.println(q.remove());
+		System.out.println(q.remove());
+		System.out.println(q.remove());
+		System.out.println(q.remove());
+		System.out.println(q.remove());
+		System.out.println(q.remove());
+		System.out.println(q.remove());
+		System.out.println(q.remove());
+		System.out.println(q.remove());
+		System.out.println(q.remove());
+		System.out.println(q.remove());
+		System.out.println(q.remove());
 		
-		System.out.println(m1.getPriority());
-		System.out.println(m2.getPriority());
-		System.out.println(m3.getPriority());
-		System.out.println(m4.getPriority());
+		
+		System.out.println(m41.getArrivalTime());
+		System.out.println(m32.getArrivalTime());
+		System.out.println(m23.getArrivalTime());
+		System.out.println(m14.getArrivalTime());
+		System.out.println(m02.getArrivalTime());
 		
 		System.out.println(q.arrivalTime);
 	}
